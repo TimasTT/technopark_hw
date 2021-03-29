f7ba96e7880ea0122993d2d94060e09342b84482
Timofey Konoplyov <timofey.konoplyv@gmail.com>
2021-03-30 00:01:14 +0300

    add gitignore

--- cmake-build-debug/thirdparty/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/timas/technopark_hw/technopark_cpp_hw/technopark_hw/thirdparty
-# Build directory: /home/timas/technopark_hw/technopark_cpp_hw/technopark_hw/cmake-build-debug/thirdparty
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/thirdparty/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/timas/technopark_hw/technopark_cpp_hw/technopark_hw/thirdparty/googletest
-# Build directory: /home/timas/technopark_hw/technopark_cpp_hw/technopark_hw/cmake-build-debug/thirdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
