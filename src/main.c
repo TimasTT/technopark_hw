237fc158d71c271a581346c681983a14019f74a8
Timofey Konoplyov <timofey.konoplyv@gmail.com>
2021-04-06 23:11:25 +0300

    checking file fixes

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "file_utils.h"
 
 int main() {
-    int file_size = 10 * 1024 * 1024;
+    int file_size = 100 * 1024 * 1024;
     const char* filename = "my_file";
 
     fill_file_random(filename, file_size);
